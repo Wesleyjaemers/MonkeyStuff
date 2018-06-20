a6b3d81246825b30cbc195c94ed33c7e076474db
Wesleyjaemers <jaemers.wesley@gmail.com>
2018-06-20 16:01:56 +0200

    add firstApp

---
+++ src/main/java/be/wesley/game/HelloJME3.java
@@ -0,0 +1,33 @@
+package be.wesley.game;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.shape.Sphere;
+
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys. */
+
+public class HelloJME3 extends SimpleApplication {
+
+    public static void main(String[] args){
+        HelloJME3 app = new HelloJME3();
+        app.start(); // start the game
+    }
+
+    @Override
+    public void simpleInitApp() {
+        //Box b = new Box(1, 1, 1); // create cube shape
+        Sphere s = new Sphere (500,500, 20);
+
+        Geometry geom = new Geometry("Sphere", s);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+}
--- src/main/java/be/wesley/game/MyGame.java
+++
@@ -1,7 +0,0 @@
-package be.wesley.game;
-
-public class MyGame {
-    public static void main(String[] args) {
-        System.out.println("Welkom to my game");
-    }
-}
